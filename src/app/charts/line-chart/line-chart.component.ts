efb6fc8fccacb97b637e7c8e0349ee457afaafbf
Pro Grammer <hamzagigran@gmail.com>
2020-09-04 12:16:41 +0100

    modifivations in line chart

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -20,15 +20,15 @@ export class LineChartComponent implements OnInit {
     this.chartsService.getCovidData()
       .subscribe(data1 => {
         let array = [
-          ['mois', 'Cas confirmés ', 'Negative tests', 'Retablis','Décédés'],         
+          ['mois', 'Cas confirmés ', 'Negative tests', 'Retablis', 'Décédés'],
         ];
         data1['features'].forEach(eliment => {
           array.push([this.convert(eliment['properties'].Date), eliment['properties']
             .Retablis, eliment['properties'].Negative_tests,
-            eliment['properties']
-              .Cas_confirmés, eliment['properties']
-              .Décédés]
-              )
+          eliment['properties']
+            .Cas_confirmés, eliment['properties']
+            .Décédés]
+          )
         });
         for (let i = 0; i < array.length; i++) {
           this.dataCovid.push(array[i]);
@@ -41,11 +41,54 @@ export class LineChartComponent implements OnInit {
       });
     let chart = new this.gLib.visualization.LineChart(document.getElementById('divLineChart'));
   }
-   convert(str) {
+  convert(str) {
     var date = new Date(str),
       mnth = ("0" + (date.getMonth() + 1)).slice(-2),
       day = ("0" + date.getDate()).slice(-2);
-      return [ mnth, day].join("-");
+    switch (mnth) {
+      case "01": {
+        return ["jan", day].join(" ");
+        break;
+      }
+      case "02": {
+        return ["Feb", day].join(" ");
+        break;
+      }
+      case "03": {
+        return ["mar", day].join(" ");
+      }
+      case "04": {
+        return ["apr", day].join(" ");
+      }
+      case "05": {
+        return ["may", day].join(" ");
+      }
+      case "06": {
+        return ["jun", day].join(" ");
+      }
+      case "07": {
+        return ["jul", day].join(" ");
+      }
+      case "08": {
+        return ["aug", day].join(" ");
+      }
+      case "09": {
+        return ["sep", day].join(" ");
+      }
+      case "10": {
+        return ["oct", day].join(" ");
+      }
+      case "11": {
+        return ["nov", day].join(" ");
+      }
+      case "12": {
+        return ["dec", day].join(" ");
+      }
+
+
+    }
+
+
   }
 
 }
